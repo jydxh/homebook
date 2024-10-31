456498ad8ee065f0e56c2e782d31ed4718f40a5e
jydxh <wzxxds@gmail.com>
2024-10-31 10:16:38 -0400

    1

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -13,6 +13,8 @@ import { TCountryCode } from "countries-list";
 import AddFav from "@/components/form/AddFav";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
+import PropertyListFallBack from "@/components/fallback/PropertyListFallBack";
+import { Suspense } from "react";
 async function FavoritesPage() {
 	const properties = await fetchFavProperties();
 	//console.log(properties);
@@ -30,44 +32,46 @@ async function FavoritesPage() {
 				My Favourite property Lists
 			</h2>
 			<Separator className="mt-8" />
-			<section className="p-8 gap-x-8 gap-y-16 grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-				{properties.map(item => {
-					const { id, country, image, name, price, tagline, latLng } = item;
-					const images = JSON.parse(image as string) as string[];
-					const isFav = favList.includes(id);
-					return (
-						<Card
-							key={id}
-							className="relative mx-auto rounded w-[75%] md:w-full bg-stone-50 hover:bg-muted ">
-							<Link href={`/properties/${id}`} className="z-0">
-								{/* carousel */}
-								<CarouselImages images={images} name={name} />
-								<div className="px-2 pb-2">
-									<div className="flex justify-between mt-2">
-										<p className="font-medium capitalize">{name}</p>
-										<div className="flex items-center gap-x-2">
-											<FaStar className="w-4 h-4" />
-											<span>5.0 </span>
+			<Suspense fallback={<PropertyListFallBack />}>
+				<section className="p-8 gap-x-8 gap-y-16 grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+					{properties.map(item => {
+						const { id, country, image, name, price, tagline, latLng } = item;
+						const images = JSON.parse(image as string) as string[];
+						const isFav = favList.includes(id);
+						return (
+							<Card
+								key={id}
+								className="relative mx-auto rounded w-[75%] md:w-full bg-stone-50 hover:bg-muted ">
+								<Link href={`/properties/${id}`} className="z-0">
+									{/* carousel */}
+									<CarouselImages images={images} name={name} />
+									<div className="px-2 pb-2">
+										<div className="flex justify-between mt-2">
+											<p className="font-medium capitalize">{name}</p>
+											<div className="flex items-center gap-x-2">
+												<FaStar className="w-4 h-4" />
+												<span>5.0 </span>
+											</div>
 										</div>
+										<p className="text-muted-foreground truncate">{tagline}</p>
+										<div className="flex justify-between">
+											{/* xxx km away */}
+											<DistanceAway latLng={latLng} />
+											<CountryAndFlag country={country as TCountryCode} />
+										</div>
+										<p className="font-medium">
+											{formatCurrency(price)} per night
+										</p>
 									</div>
-									<p className="text-muted-foreground truncate">{tagline}</p>
-									<div className="flex justify-between">
-										{/* xxx km away */}
-										<DistanceAway latLng={latLng} />
-										<CountryAndFlag country={country as TCountryCode} />
-									</div>
-									<p className="font-medium">
-										{formatCurrency(price)} per night
-									</p>
+								</Link>
+								<div className="absolute top-1 right-1">
+									<AddFav propertyId={id} isFav={isFav} />
 								</div>
-							</Link>
-							<div className="absolute top-1 right-1">
-								<AddFav propertyId={id} isFav={isFav} />
-							</div>
-						</Card>
-					);
-				})}
-			</section>
+							</Card>
+						);
+					})}
+				</section>
+			</Suspense>
 		</>
 	);
 }
