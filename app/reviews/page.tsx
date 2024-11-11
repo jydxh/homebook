7b39517395bfc73937adb7c86314019f3c8aa21c
jydxh <wzxxds@gmail.com>
2024-11-11 10:55:03 -0500

    make review card clickable to navigate to property details

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -84,21 +84,28 @@ async function ReviewCard({
 	return (
 		<Card>
 			{/* image and title and create at */}
-			<div className="flex items-center gap-x-4">
-				<div className="relative w-[140px] h-[140px] rounded">
-					<Image fill src={image} alt={name} className="object-cover rounded" />
-				</div>
-				<div>
-					<p className="font-medium text-lg truncate">{name}</p>
-					<div className="flex items-center gap-x-4 flex-wrap">
-						<CountryAndFlag country={country as TCountryCode} />
-						<PropertyRating propertyId={propertyId} />
+			<Link href={`/properties/${propertyId}`}>
+				<div className="flex items-center gap-x-4">
+					<div className="relative w-[140px] h-[140px] rounded">
+						<Image
+							fill
+							src={image}
+							alt={name}
+							className="object-cover rounded"
+						/>
 					</div>
-					<div className="text-muted-foreground">
-						{formatDate({ date: createAt })}
+					<div>
+						<p className="font-medium text-lg truncate">{name}</p>
+						<div className="flex items-center gap-x-4 flex-wrap">
+							<CountryAndFlag country={country as TCountryCode} />
+							<PropertyRating propertyId={propertyId} />
+						</div>
+						<div className="text-muted-foreground">
+							{formatDate({ date: createAt })}
+						</div>
 					</div>
 				</div>
-			</div>
+			</Link>
 
 			<Separator className="mb-4" />
 			{/*rating*/}
