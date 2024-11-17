112121e64fe8b4c59e0e4a5ae5c5168a78f0e78a
jydxh <wzxxds@gmail.com>
2024-11-17 00:47:45 -0500

    add an alert for delete rental btn

--- components/rentals/DeleteBtn.tsx
+++ components/rentals/DeleteBtn.tsx
@@ -1,7 +1,18 @@
-import { Button } from "../ui/button";
 import FormContainer from "../form/FormContainer";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { deleteRentalAction } from "@/utils/actions/PropertyActions";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 function DeleteBtn({
 	id,
@@ -12,11 +23,30 @@ function DeleteBtn({
 }) {
 	const deleteRental = deleteRentalAction.bind(null, {}, { id, searchParams });
 	return (
-		<FormContainer action={deleteRental}>
-			<Button variant="outline" size={"icon"}>
-				<RiDeleteBinLine className="w-4 h-4 hover:text-primary" />
-			</Button>
-		</FormContainer>
+		<>
+			<AlertDialog>
+				<AlertDialogTrigger asChild>
+					<Button variant="outline" size={"icon"}>
+						<RiDeleteBinLine className="w-4 h-4 hover:text-primary" />
+					</Button>
+				</AlertDialogTrigger>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>Warning</AlertDialogTitle>
+						<AlertDialogDescription>
+							This action cannot be undone. This will permanently delete your
+							rental and remove the data from our servers.
+						</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogCancel>Cancel</AlertDialogCancel>
+						<FormContainer action={deleteRental}>
+							<AlertDialogAction type="submit">Delete</AlertDialogAction>
+						</FormContainer>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+		</>
 	);
 }
 export default DeleteBtn;
