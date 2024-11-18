33109039f5bce4bf0edd80688a5be85a687c06ec
jydxh <wzxxds@gmail.com>
2024-11-18 09:52:49 -0500

    cloudinary delete img by public_Id function

--- utils/cloudinaryUpload.ts
+++ utils/cloudinaryUpload.ts
@@ -18,9 +18,26 @@ export default async function cloudinaryUpload(file: File): Promise<string> {
 				folder: "Airbnb",
 			}
 		);
+		console.log(uploadResult.public_id);
 		return uploadResult.secure_url;
 	} catch (error) {
 		console.log(error);
 		throw new Error("Cloudinary config error");
+	}
+}
+
+export async function cloudinaryDelete(public_id: string) {
+	cloudinary.config({
+		cloud_name: "dudordakm",
+		api_key: process.env.CLOUDINARY_API_KEY,
+		api_secret: process.env.CLOUDINARY_SECRET, // Click 'View API Keys' above to copy your API secret
+	});
+	try {
+		const result = await cloudinary.uploader.destroy(public_id);
+		console.log(result);
+		return { result: "deleted" };
+	} catch (error) {
+		console.log(error);
+		throw new Error("failed to delete");
 	}
 }
