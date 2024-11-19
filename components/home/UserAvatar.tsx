f7e1a35ee930199a8d23ada8656dc2988866f555
jydxh <wzxxds@gmail.com>
2024-11-18 23:58:10 -0500

    fix the userAvatar potential bugs

--- components/home/UserAvatar.tsx
+++ components/home/UserAvatar.tsx
@@ -6,22 +6,19 @@ import { LuUser2 } from "react-icons/lu";
 
 async function UserAvatar() {
 	const user = await currentUser();
+	if (!user)
+		return (
+			<LuUser2 className="w-[45px] h-[45px] bg-primary rounded-full text-white border-2 hover:border-none border-slate-400/75" />
+		);
 	const profile = await fetchUserProfile();
-
 	return (
-		<>
-			{!user ? (
-				<LuUser2 className="w-[45px] h-[45px] bg-primary rounded-full text-white border-2 hover:border-none border-slate-400/75" />
-			) : (
-				<Image
-					src={profile?.profileImage || user.imageUrl}
-					alt="avatar"
-					width={98}
-					height={98}
-					className="w-[45px] h-[45px] rounded-full object-cover border-2 hover:border-none border-slate-400/75"
-				/>
-			)}
-		</>
+		<Image
+			src={profile?.profileImage || user.imageUrl}
+			alt="avatar"
+			width={98}
+			height={98}
+			className="w-[45px] h-[45px] rounded-full object-cover border-2 hover:border-none border-slate-400/75"
+		/>
 	);
 }
 export default UserAvatar;
