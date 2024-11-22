afea379fd5afbb05381f3914fcf87d3d990f2072
jydxh <wzxxds@gmail.com>
2024-11-22 09:33:55 -0500

    update the login dialog

--- components/form/AddFav.tsx
+++ components/form/AddFav.tsx
@@ -4,12 +4,21 @@ import FormContainer from "./FormContainer";
 import { addFavAction } from "@/utils/actions/PropertyActions";
 import { usePathname } from "next/navigation";
 import { AddFavButton } from "./Buttons";
-import { useAuth } from "@clerk/nextjs";
-import { Button } from "../ui/button";
+import { useAuth, SignIn } from "@clerk/nextjs";
+
 import { IoIosHeartEmpty } from "react-icons/io";
 import { useEffect, useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import {
+	AlertDialog,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogTrigger,
+	AlertDialogCancel,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import DemoUserBtn from "../home/DemoUserBtn";
 
-import Link from "next/link";
 function AddFav({
 	isFav,
 	propertyId,
@@ -38,20 +47,13 @@ function AddFav({
 			</Button>
 		);
 	}
-	if (!userId)
-		return (
-			<Link href={`/sign-in?fallbackPath=${path}`}>
-				<Button type="button" size="icon" variant="outline">
-					<IoIosHeartEmpty className="w-5 h-5" />
-				</Button>
-			</Link>
-		);
+	if (!userId) return <LoginDialog path={path} />;
 	{
-		/* <SignInButton fallbackRedirectUrl={path}>
+		/* 	<Link href={`/sign-in?fallbackPath=${path}`}>
 				<Button type="button" size="icon" variant="outline">
 					<IoIosHeartEmpty className="w-5 h-5" />
 				</Button>
-			</SignInButton> */
+			</Link> */
 	}
 
 	return (
@@ -61,3 +63,30 @@ function AddFav({
 	);
 }
 export default AddFav;
+
+function LoginDialog({ path }: { path: string }) {
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button type="button" size="icon" variant="outline">
+					<IoIosHeartEmpty className="w-5 h-5" />
+				</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent className="flex flex-col items-center justify-center">
+				<div className="flex flex-row-reverse w-full">
+					<AlertDialogCancel className="p-2">
+						<AiOutlineCloseCircle className="w-5 h-5" />
+					</AlertDialogCancel>
+				</div>
+				<SignIn
+					routing="hash"
+					fallbackRedirectUrl={path || "/"}
+					signUpFallbackRedirectUrl={path || "/"}
+				/>
+				<AlertDialogFooter>
+					<DemoUserBtn />
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
