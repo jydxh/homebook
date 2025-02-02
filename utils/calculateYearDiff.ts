a593c6f8e7d6cf16b4a3efc0bcaeea15803a6c1c
jydxh <wzxxds@gmail.com>
2025-02-01 19:43:59 -0500

    refactor calculateYearDiff function for improved date difference calculation

--- utils/calculateYearDiff.ts
+++ utils/calculateYearDiff.ts
@@ -4,14 +4,29 @@ function calculateYearDiff(prevDate: Date) {
 	const diff = now.getTime() - prev.getTime();
 	const oneYearInSeconds = 365 * 24 * 60 * 60 * 1000;
 	const oneMonthInSeconds = 30 * 24 * 60 * 60 * 1000;
-	const yearDiff = now.getFullYear() - prev.getFullYear();
-	const monthDiff = now.getMonth() - prev.getMonth();
-	if (diff > oneYearInSeconds) {
-		return yearDiff + (yearDiff > 1 ? " years" : " year");
-	} else if (diff < oneMonthInSeconds) {
-		return "in 1 month";
+
+	const timeDiffInMonths = Math.floor(diff / oneMonthInSeconds);
+	const timeDiffInYears = Math.floor(diff / oneYearInSeconds);
+	const remainingMonths = timeDiffInMonths % 12;
+
+	if (timeDiffInYears < 1) {
+		return `${timeDiffInMonths} ${timeDiffInMonths > 1 ? "months" : "month"}`;
 	} else {
-		return monthDiff + (monthDiff > 1 ? " months" : " month");
+		return `${timeDiffInYears} ${timeDiffInYears > 1 ? "years" : "year"}${
+			remainingMonths > 0
+				? ` and ${remainingMonths} ${remainingMonths > 1 ? "months" : "month"}`
+				: ""
+		}`;
 	}
+
+	// const yearDiff = now.getFullYear() - prev.getFullYear();
+	// const monthDiff = now.getMonth() - prev.getMonth();
+	// if (diff > oneYearInSeconds) {
+	// 	return yearDiff + (yearDiff > 1 ? " years" : " year");
+	// } else if (diff < oneMonthInSeconds) {
+	// 	return "in 1 month";
+	// } else {
+	// 	return monthDiff + (monthDiff > 1 ? " months" : " month");
+	// }
 }
 export default calculateYearDiff;
